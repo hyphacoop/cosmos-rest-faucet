cf7c465cb08bf71b42838b2569f0725fa572d8d9
danb <dan@bryanlabs.net>
2022-11-28 18:19:07 -0500

    faucet changes

--- gaia_calls.py
+++ gaia_calls.py
@@ -17,7 +17,7 @@ async def check_address(address: str, gaia_home: str = '~/.strange'):
     """
     check = subprocess.run(["stranged", "keys", "parse",
                             f"{address}",
-                            f'--home={gaia_home}',
+                            f'--home=/root/.strange',
                             '--output=json'],
                            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                            text=True)
@@ -42,7 +42,7 @@ async def get_balance_list(address: str, node: str, gaia_home: str = '~/.strange
     balance = subprocess.run(["stranged", "query", "bank", "balances",
                               f"{address}",
                               f'--node={node}',
-                              f'--home={gaia_home}',
+                              f'--home=/root/.strange',
                               '--output=json'],
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                              text=True)
@@ -74,17 +74,22 @@ async def tx_send(request: dict):
                        --keyring-backend=test -y
 
     """
-    tx_gaia = subprocess.run(['stranged', 'tx', 'bank', 'send',
+
+    cmd = ['stranged', 'tx', 'bank', 'send',
                               f'{request["sender"]}',
                               f'{request["recipient"]}',
                               f'{request["amount"]}',
-                              f'--fees={request["fees"]}',
                               f'--node={request["node"]}',
                               f'--chain-id={request["chain_id"]}',
-                              f'--home={request["gaia_home"]}',
+                              f'--home=/root/.strange',
                               '--keyring-backend=test',
+                              '--gas-prices=0.0025ustrange',
+                              '--gas-adjustment=1.2',
                               '--output=json',
-                              '-y'],
+                              '-y']
+    logging.info(" ".join(cmd))
+    print(" ".join(cmd))
+    tx_gaia = subprocess.run(cmd,
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     try:
         tx_gaia.check_returncode()
